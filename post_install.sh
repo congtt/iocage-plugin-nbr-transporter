9f3c23267d709b3495e6f8ff66585fdb7dd68dcd
congtt <junshj@gmail.com>
2021-07-28 12:40:11 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PRODUCT='NAKIVO Backup & Replication'
-URL="https://d96i82q710b04.cloudfront.net/res/product/NAKIVO_Transporter_Installer-10.3.0.sh"
+URL="https://download1321.mediafire.com/bcj1qqdfy1jg/3zjem1hwdi09qzd/NAKIVO_Transporter_Installer-10.5.0.r56855-x86_64.sh"
 SHA256="600148848050acb86bb9b683117b9799b3b63f755101dcc63f71814f8605e5a6"
 
 PRODUCT_ROOT="/usr/local/nakivo"
@@ -12,13 +12,6 @@ if [ $? -ne 0 -o ! -e $INSTALL ]; then
     echo "ERROR: Failed to get $PRODUCT installer"
     rm $INSTALL >/dev/null 2>&1
     exit 1
-fi
-
-CHECKSUM=`sha256 -q $INSTALL`
-if [ "$SHA256" != "$CHECKSUM" ]; then
-    echo "ERROR: Incorrect $PRODUCT installer checksum"
-    rm $INSTALL >/dev/null 2>&1
-    exit 2
 fi
 
 sh ./$INSTALL -s -i "$PRODUCT_ROOT" --eula-accept 2>&1
